bca29bb2be2b4a425ece54e69a2ab5f723e68d2e
anesu <anesukafesu@gmail.com>
2023-09-17 04:45:45 +0300

    fixed tests

--- tests/test_models/test_engines/test_file_storage.py
+++ tests/test_models/test_engines/test_file_storage.py
@@ -1,49 +1,41 @@
+#!/usr/bin/python3
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
+from models import storage
 import unittest
 import os
 
 
 class TestFileStorage(unittest.TestCase):
 
-    def setUp(self):
-        self.storage = FileStorage()
-
     def tearDown(self):
-        if os.path.exists(self.storage._FileStorage__file_path):
-            os.remove(self.storage._FileStorage__file_path)
+        if os.path.exists(storage._FileStorage__file_path):
+            os.remove(storage._FileStorage__file_path)
 
     def test_all(self):
         # Ensure the __objects dictionary is initially empty
-        self.assertEqual(self.storage.all(), {})
+        self.assertEqual(storage.all(), {})
 
     def test_new_and_all(self):
         # Create a BaseModel instance
         model = BaseModel()
         # Add it to storage
-        self.storage.new(model)
+        storage.new(model)
         # Ensure the model is in the __objects dictionary
-        self.assertIn(f"BaseModel.{model.id}", self.storage.all())
+        self.assertIn(f"BaseModel.{model.id}", storage.all())
 
     def test_save_and_reload(self):
         # Create a BaseModel instance
         model = BaseModel()
         # Add it to storage
-        self.storage.new(model)
+        storage.new(model)
         # Save the data to a file
-        self.storage.save()
+        storage.save()
         # Reload the data from the file
         new_storage = FileStorage()
         new_storage.reload()
         # Ensure the reloaded data contains the model
         self.assertIn(f"BaseModel.{model.id}", new_storage.all())
-
-    def test_save_and_reload_nonexistent_file(self):
-        # Attempt to reload data from a nonexistent file
-        new_storage = FileStorage()
-        new_storage.reload()
-        # Ensure the result is an empty dictionary
-        self.assertEqual(new_storage.all(), {})
 
 
 if __name__ == '__main__':
