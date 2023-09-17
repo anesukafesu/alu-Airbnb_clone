448240fee0e8ad17a509edda61e289df1c174a45
anesu <anesukafesu@gmail.com>
2023-09-17 03:52:50 +0300

    removed testfile from tests

--- tests/test_models/test_engines/test_file_storage.py
+++ tests/test_models/test_engines/test_file_storage.py
@@ -1,42 +1,48 @@
+from models.file_storage import FileStorage
+from models.base_model import BaseModel
 import unittest
 import os
-import tempfile
-import shutil
-from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
+
     def setUp(self):
-        # Create a temporary directory for testing
-        self.test_dir = tempfile.mkdtemp()
-        # Set the file path for testing
-        self.test_file_path = os.path.join(self.test_dir, "test_storage.json")
-        FileStorage._FileStorage__file_path = self.test_file_path
+        self.storage = FileStorage()
 
     def tearDown(self):
-        # Remove the temporary directory and its contents
-        shutil.rmtree(self.test_dir)
+        if os.path.exists(self.storage._FileStorage__file_path):
+            os.remove(self.storage._FileStorage__file_path)
+
+    def test_all(self):
+        # Ensure the __objects dictionary is initially empty
+        self.assertEqual(self.storage.all(), {})
 
     def test_new_and_all(self):
-        storage = FileStorage()
-        data = {'__class__': 'TestObject', 'id': '1', 'name': 'Test'}
-        storage.new(data)
-        self.assertEqual(storage.all(), {"TestObject.1": data})
+        # Create a BaseModel instance
+        model = BaseModel()
+        # Add it to storage
+        self.storage.new(model)
+        # Ensure the model is in the __objects dictionary
+        self.assertIn(f"BaseModel.{model.id}", self.storage.all())
 
     def test_save_and_reload(self):
-        storage = FileStorage()
-        data = {'__class__': 'TestObject', 'id': '1', 'name': 'Test'}
-        storage.new(data)
-        storage.save()
+        # Create a BaseModel instance
+        model = BaseModel()
+        # Add it to storage
+        self.storage.new(model)
+        # Save the data to a file
+        self.storage.save()
+        # Reload the data from the file
         new_storage = FileStorage()
         new_storage.reload()
-        self.assertEqual(new_storage.all(), {"TestObject.1": data})
+        # Ensure the reloaded data contains the model
+        self.assertIn(f"BaseModel.{model.id}", new_storage.all())
 
     def test_save_and_reload_nonexistent_file(self):
-        storage = FileStorage()
-        storage.save()
+        # Attempt to reload data from a nonexistent file
         new_storage = FileStorage()
         new_storage.reload()
+        # Ensure the result is an empty dictionary
         self.assertEqual(new_storage.all(), {})
 
 
