0f01c0573d22ac1628c545e972eed1398d2046b6
anesu <anesukafesu@gmail.com>
2023-09-17 03:10:22 +0300

    fixed mistake

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,52 +1,47 @@
-import unittest
-import os
+#!/usr/bin/python3
+"""Module for FileStorage Engine
+"""
+from json import dump, load
 from models.base_model import BaseModel
-from models.file_storage import FileStorage
 
 
-class TestFileStorage(unittest.TestCase):
+class FileStorage:
+    """FileStorage class used to serialise objects to JSON files
+    """
+    __file_path = "data.json"
+    __objects = {}
 
-    def setUp(self):
-        self.storage = FileStorage()
-        self.test_file_path = "test_data.json"
-        self.storage._FileStorage__file_path = self.test_file_path
+    def all(self):
+        return self.__objects
 
-    def tearDown(self):
-        if os.path.exists(self.test_file_path):
-            os.remove(self.test_file_path)
+    def new(self, obj):
+        # Creating a key to store the object
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
 
-    def test_all(self):
-        # Ensure the __objects dictionary is initially empty
-        self.assertEqual(self.storage.all(), {})
+        # Adding the object to our dictionary of objects
+        self.__objects[key] = obj
 
-    def test_new_and_all(self):
-        # Create a BaseModel instance
-        model = BaseModel()
-        # Add it to storage
-        self.storage.new(model)
-        # Ensure the model is in the __objects dictionary
-        self.assertIn(f"BaseModel.{model.id}", self.storage.all())
+    def save(self):
+        # Convert objects to dictionaries
+        object_dictionaries = {}
+        for key, value in self.__objects.items():
+            object_dictionaries[key] = value.to_dict()
 
-    def test_save_and_reload(self):
-        # Create a BaseModel instance
-        model = BaseModel()
-        # Add it to storage
-        self.storage.new(model)
-        # Save the data to a file
-        self.storage.save()
-        # Reload the data from the file
-        new_storage = FileStorage()
-        new_storage.reload()
-        # Ensure the reloaded data contains the model
-        self.assertIn(f"BaseModel.{model.id}", new_storage.all())
+        # Writing the object dictionaries to file
+        with open(self.__file_path, "w+") as f:
+            dump(object_dictionaries, f)
 
-    def test_save_and_reload_nonexistent_file(self):
-        # Attempt to reload data from a nonexistent file
-        new_storage = FileStorage()
-        new_storage.reload()
-        # Ensure the result is an empty dictionary
-        self.assertEqual(new_storage.all(), {})
+    def reload(self):
+        try:
+            with open(self.__file_path, "r") as f:
 
+                # Parse the objects from JSON
+                object_dictionaries = load(f)
 
-if __name__ == '__main__':
-    unittest.main()
+                # Convert them to BaseModels
+                for key, value in object_dictionaries.items():
+                    class_name = key.split(".")[0]
+                    self.__objects[key] = eval(class_name)(**value)
+
+        except FileNotFoundError:
+            pass
